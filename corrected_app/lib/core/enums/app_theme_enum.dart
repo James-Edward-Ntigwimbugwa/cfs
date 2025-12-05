3cc48922df1714ca52748a364988717dac3008aa
James Edward <jamesnavigator04@gmail.com>
2025-12-06 02:03:27 +0300

    ADMIN DASHBOARD: Fix import errors

--- corrected_app/lib/core/enums/app_theme_enum.dart
+++
@@ -1,2 +0,0 @@
-enum AppThemeMode { system, light, dark }
-
--- corrected_app/lib/shared/utilities/helper_methods/name_initials.dart
+++
@@ -1,7 +0,0 @@
-String generateInitials(String name) {
-  final parts = name.split(' ');
-  if (parts.length >= 2) {
-    return '${parts[0][0]}${parts[1][0]}'.toUpperCase();
-  }
-  return name.isNotEmpty ? name[0].toUpperCase() : '?';
-}
